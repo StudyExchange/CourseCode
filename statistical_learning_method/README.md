b1128070fd143aba17f9999ffde4f6fe49761e82
se <StudyExchange@163.com>
2018-01-31 19:23:55 +0800

    [se] statistical_learning_method, algorithm 5.4, paused

--- statistical_learning_method/README.md
+++ statistical_learning_method/README.md
@@ -1,13 +1,14 @@
 # 统计学习方法，李航著
 代码实现与书中算法的具体步骤对应很紧密，便于理解算法。但，缺点是，1）代码实现不严谨，对于很多异常情况并没有做处理。2）代码实用性差，没有都做成model.train和model.predict的形式，只是简单实现，验证例题中的结果。
 
-软件环境：Anaconda、Python=3.6、Jupyter Notebook。比较复杂的代码，也使用VS Code来调试。
+软件环境：Anaconda、Python=3.6、Jupyter Notebook。比较复杂的代码，也使用Visual Studio Code来调试。
 
 |  项目  |  数量  |
 |:------:|:------:|
-| 未实现 |   33   |
-| 已实现 |    7   |
+| 无实现 |    5   |
+| 未实现 |   27   |
 | 还没懂 |    1   |
+| 已实现 |    8   |
 |  合计  |   41   |
 
 
@@ -15,7 +16,7 @@
 - 算法 2.1（感知机学习算法的原始形式）algorithm 2.1(perceptron learning algorithm of primal form)
 - 算法 2.2（感知机学习算法的对偶形式）algorithm 2.2(perceptron learning algorithm of dual form)
 ## 第三章 k近邻法
-- 算法 3.1（k近邻法）【未实现】
+- 算法 3.1（k近邻法）【无实现】
 - 算法 3.2（构造平衡kd树）algorithm 3.2(build balance kd tree)
 - 算法 3.3（用kd树的最近邻搜索）【还没懂】
 ## 第四章 朴素贝叶斯法
@@ -24,18 +25,18 @@
 - 算法 5.1（信息增益的算法）algorithm 5.1(information gain algorithm)
 - 算法 5.2（ID3算法）algorithm 5.2(ID3 algorithm)
 - 算法 5.3（C4.5的生成算法）algorithm 5.3(C4.5 generative algorithm)
-- 算法 5.4（树的剪枝算法）
-- 算法 5.5（最小二乘回归树生成算法）
-- 算法 5.6（CART生成算法）
-- 算法 5.7（CART剪枝算法）
+- 算法 5.4（树的剪枝算法）algorithm 5.4(the pruning algorithm of tree)
+- 算法 5.5（最小二乘回归树生成算法）algorithm 5.5(least squares regression tree generative algorithm)
+- 算法 5.6（CART生成算法）algorithm 5.6(CART generative algorithm)
+- 算法 5.7（CART剪枝算法）algorithm 5.7(CART pruning algorithm)
 ## 第六章 逻辑斯蒂回归与最大熵模型
 - 算法 6.1（改进的迭代尺度算法IIS）
 - 算法 6.2（最大熵学习的BFGS算法）
 ## 第七章 支持向量机
-- 算法 7.1（线性可分支持向量机学习算法——最大间隔法）【未实现】
-- 算法 7.2（线性可分支持向量机学习算法）【未实现】
-- 算法 7.3（线性支持向量机学习算法）【未实现】
-- 算法 7.4（非线性支持向量机学习算法）【未实现】
+- 算法 7.1（线性可分支持向量机学习算法——最大间隔法）【无实现】
+- 算法 7.2（线性可分支持向量机学习算法）【无实现】
+- 算法 7.3（线性支持向量机学习算法）【无实现】
+- 算法 7.4（非线性支持向量机学习算法）【无实现】
 - 算法 7.5（SMO算法）algorithm 7.5(SMO algorithm)
 ## 第八章 提升方法
 - 算法 8.1（AdaBoost）
--- statistical_learning_method/debug.py
+++ statistical_learning_method/debug.py
@@ -1,235 +1,334 @@
+import pdb
+
 import numpy as np
-import random
+import pandas as pd
+from IPython.display import display
 
-class Smo(object):
-    def __init__(self, c=10):
-        # self._w = None
-        self._b = 0
-        self._c = c
-        self._epsilon = 0.001
-        # self._xi = 0.001
-        self._max_interaction = 100
+# 表5.1 贷款申请样本数据表
+loan_application = [
+    (1, '青年', '否', '否', '一般', '否'),
+    (2, '青年', '否', '否', '好', '否'),
+    (3, '青年', '是', '否', '好', '是'),
+    (4, '青年', '是', '是', '一般', '是'),
+    (5, '青年', '否', '否', '一般', '否'),
+    (6, '中年', '否', '否', '一般', '否'),
+    (7, '中年', '否', '否', '好', '否'),
+    (8, '中年', '是', '是', '好', '是'),
+    (9, '中年', '否', '是', '非常好', '是'),
+    (10, '中年', '否', '是', '非常好', '是'),
+    (11, '老年', '否', '是', '非常好', '是'),
+    (12, '老年', '否', '是', '好', '是'),
+    (13, '老年', '是', '否', '好', '是'),
+    (14, '老年', '是', '否', '非常好', '是'),
+    (15, '老年', '否', '否', '一般', '否')
+]
 
-    def train(self, x, y):
-        self._init_parameters(x, y)
-        for i in range(self._max_interaction):
-            # 【参照@WenDesi的实现】把0<alpha<C的alpha放在list的前面，剩余的放在后面。
-            # 这样实现p129第1段中描述。
-            indexes = list(range(self._m))
-            condidate_indexes = list(filter(lambda i: self._alpha[i] > 0 and self._alpha[i] < self._c, indexes))
-            remaining_indexes = list(set(indexes) - set(condidate_indexes))
-            entire_list = condidate_indexes + remaining_indexes
-            for j in entire_list:
-                if self._is_stop():
-                    print('Finished at time: %s' % i)
-                    return
-                i1 = j
-                i2 = self._choose_second_parameter(i1)
-                print(i1, i2)
-                # 【p126，最后一段，求L和H的公式】
-                if self._y[i1] != self._y[i2]:
-                    low = max(0, self._alpha[i2] - self._alpha[i1])
-                    high = min(self._c, self._c + self._alpha[i2] - self._alpha[i1])
-                else:
-                    low = max(0, self._alpha[i2] + self._alpha[i1] - self._c)
-                    high = min(self._c, self._alpha[i2] + self._alpha[i1])
-                if low == high:
-                    continue
-                # 【p127，公式7.107】eta = K11 + K12 - 2K12
-                eta = self._k(self._x[i1], self._x[i1]) + self._k(self._x[i2], self._x[i2]) - 2 * self._k(self._x[i1], self._x[i2])
-                # 【p127，公式7.106】未经剪辑的alpha2_new
-                alpha2_new = self._alpha[i2] + self._y[i2] * (self._error(i1) - self._error(i2)) / eta
-                # 剪辑alpha2_new
-                # alpha2_new = self._clip_alpha2_new(alpha2_new, low, high)
-                # 【p127，公式7.108】
-                alpha2_new = min(high, alpha2_new)
-                alpha2_new = max(low, alpha2_new)
-                # 【p127，公式7.109】有alpha2_new求alpha1_new
-                alpha1_new = self._alpha[i1] + self._y[i1] * self._y[i2] * (self._alpha[i2] - alpha2_new)
-                # 【p130，公式7.115】计算b2_new
-                b1_new = -self._error(i1) - self._y[i1] * self._k(self._x[i1], self._x[i1]) * (alpha1_new - self._alpha[i1]) \
-                        -self._y[i2] * self._k(self._x[i2], self._x[i1]) * (alpha2_new - self._alpha[i2]) + self._b
-                # 【p130，公式7.116】计算b1_new
-                b2_new = -self._error(i2) - self._y[i1] * self._k(self._x[i1], self._x[i2]) * (alpha1_new - self._alpha[i1]) \
-                        -self._y[i2] * self._k(self._x[i2], self._x[i2]) * (alpha2_new - self._alpha[i2]) + self._b
-                # 【p130，中部，关于b_new的取值方法】
-                if alpha1_new > 0 and alpha1_new < self._c:
-                    b_new = b1_new
-                elif alpha2_new > 0 and alpha2_new < self._c:
-                    b_new = b2_new
-                else:
-                    b_new = (b1_new + b2_new) / 2
-                self._b = b_new
-                # 更新alpha1和alpha2
-                self._alpha[i1] = alpha1_new
-                self._alpha[i2] = alpha2_new
-                print(self._alpha)
-                print(self._weight(), self._b)
-        print('max interaction')
+df = pd.DataFrame(loan_application, columns=['ID', '年龄', '有工作', '有自己的房子', '借贷情况', '类别'])
+# display(df)
 
-    def predict(self, x):
-        results = [0] * len(x)
-        for i, sample in enumerate(x):
-            # 【p124，公式7.94】分类决策函数
-            y_hat = self._b
-            for j in range(self._m):
-                temp = self._alpha[j] * self._y[j] * self._k(sample, self._x[j])
-                y_hat += temp
-            results[i] = self._sign(y_hat)
-        return results
+# 将类型转换为int类型，使用100、101、10、11、12等数字，而不是0、1、2等是为了便于在计算过程中区分各个类别
+df['类别'] = df['类别'].map({'否': 100, '是': 101}).astype(int)
+df['年龄'] = df['年龄'].map({'青年': 10, '中年': 11, '老年': 12}).astype(int)
+df['有工作'] = df['有工作'].map({'否': 20, '是': 21}).astype(int)
+df['有自己的房子'] = df['有自己的房子'].map({'否': 30, '是': 31}).astype(int)
+df['借贷情况'] = df['借贷情况'].map({'一般': 40, '好': 41, '非常好': 42})
+# display(df)
 
-    def _weight(self):
-        # 【p33，公式2.14】
-        result = np.dot(np.array(self._alpha) * np.array(self._y), np.array(self._x))
-        return result
+x_data = df[['年龄', '有工作', '有自己的房子', '借贷情况']].as_matrix()
+y_data = df['类别'].as_matrix()
 
-    def _sign(self, x):
-        if x >= 0:
-            return 1
+print(x_data)
+print(y_data)
+print(x_data.shape)
+print(y_data.shape)
+
+# （1）经验熵H(D)
+import math
+
+def empirical_entropy(labels):
+    # labels：int类型的list
+    lenght = len(labels)
+    class_names = set(labels)
+    # 用字典来统计各个值出现的次数
+    class_group = {}
+    # 初始化字典，存储各个类别的数量都为0
+    for c in class_names:
+        class_group[c] = list(labels).count(c)
+    # 汇总各个类别，【p62，公式5.7】
+#     entropies = []
+#     for g in class_group.values():
+#         entropies.append(- g / lenght * math.log2(g / lenght))
+#     entropy = sum(entropies)
+    entropy = sum([- g / lenght * math.log2(g / lenght) for g in class_group.values()]) #这一行代码等同于前面4行
+    return entropy
+
+entropy = empirical_entropy(y_data)
+print('（1）经验熵H(D) = %.3f' % entropy)
+
+# （2）条件经验熵H(D|A)
+
+def conditional_empirical_entropy(feature, labels):
+    # feature：单个特征的int类型的list，int是map之后的类别，支持多个特征取条件经验熵
+    # labels：int类型的list，int是map之后的类别
+    lenght = len(labels)
+    classes = set(feature)
+    # 按feature对数据进行分组
+    feature_group = {}
+    for c in classes:
+        indexes = [i for i in range(len(feature)) if feature[i] == c]
+        feature_group[c] = indexes
+#     print(feature_group)
+    entropies = []
+    # 【p62，公式5.8】
+    for indexes in feature_group.values():
+        di_d = len(indexes) / lenght
+        entropies.append(di_d*empirical_entropy(labels[indexes]))
+    entropy = sum(entropies)
+    return entropy
+
+entropy = conditional_empirical_entropy(x_data[:, 0], y_data)
+print('（2）条件经验熵H(D|A) = %.3f' % entropy)
+
+# （3）信息增益g(D,A)
+def information_gain(feature, labels):
+    # 【p62，公式5.9】
+    return empirical_entropy(labels) - conditional_empirical_entropy(feature, labels)
+
+gain = information_gain(x_data[:, 0], y_data)
+print('（3）信息增益g(D,A) = %.3f' % gain)
+
+# 计算各个特征的信息增益
+for i in range(x_data.shape[1]):
+    print(information_gain(x_data[:, i], y_data))
+
+def get_max_information_gain(features, labels):
+    gains = [information_gain(features[:, i], labels) for i in range(features.shape[1])]
+    sorted_indexes = np.argsort(gains)
+#     print(sorted_indexes)
+    return sorted_indexes[-1], gains[sorted_indexes[-1]]
+
+max_feature_index, max_feature_gain = get_max_information_gain(x_data, y_data)
+print('信息增益最大的index是：%s，对应特征是：A(%s)，信息增益是：%.3f' % (max_feature_index, max_feature_index + 1, max_feature_gain))
+
+# （3）信息增益比gr(D,A)
+def information_gain_ratio(feature, labels):
+    # 【p62，公式5.9】
+    return information_gain(feature, labels) / empirical_entropy(labels)
+
+gain = information_gain_ratio(x_data[:, 0], y_data)
+print('（3）信息增益比gr(D,A) = %.3f' % gain)
+
+# 计算各个特征的信息增益比
+for i in range(x_data.shape[1]):
+    print(information_gain_ratio(x_data[:, i], y_data))
+
+def get_max_information_gain_ratio(features, labels):
+    gains = [information_gain_ratio(features[:, i], labels) for i in range(features.shape[1])]
+    sorted_indexes = np.argsort(gains)
+#     print(sorted_indexes)
+    return sorted_indexes[-1], gains[sorted_indexes[-1]]
+
+max_feature_index, max_feature_gain = get_max_information_gain_ratio(x_data, y_data)
+print('信息增益最大的index是：%s，对应特征是：A(%s)，信息增益是：%.3f' % (max_feature_index, max_feature_index + 1, max_feature_gain))
+
+class Node(object):
+    def __init__(self, sub_space, labels):
+        # 如果不是叶子节点，则feature_index=index(特征再数据集中横向的索引位置), pre_label=None
+        # 如果  是叶子节点，则feature_index=None, pre_label=class
+        self._sub_space = sub_space
+        self._labels = labels
+        self._feature_index = None
+        self._pre_label = None
+        self._father_node = None
+        self._can_pruned = None # 用于保存修剪状态，可修剪：0，不可修剪：1
+        self._children = {}
+    # 属性
+    @property
+    def sub_space(self):
+        return self._sub_space
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def feature_index(self):
+        return self._feature_index
+    @feature_index.setter
+    def feature_index(self, value):
+        self._feature_index = value
+    @property
+    def pre_label(self):
+        return self._pre_label
+    @pre_label.setter
+    def pre_label(self, value):
+        self._pre_label = value
+    @property
+    def father_node(self):
+        return self._father_node
+    @father_node.setter
+    def father_node(self, value):
+        self._father_node = value
+    @property
+    def can_pruned(self):
+        return self._can_pruned
+    @can_pruned.setter
+    def can_pruned(self, value):
+        self._can_pruned = value
+
+    @property
+    def children(self):
+        return self._children
+    @children.setter
+    def children(self, value):
+        self._children = value
+    
+    # 方法
+    def is_leaf(self):
+        if self._feature_index is None and self._pre_label is not None:
+            return True
+        elif self._feature_index is not None and self._pre_label is None:
+            return False
         else:
-            return -1
+            raise Exception('节点状态异常')
 
-    def _init_parameters(self, x, y):
-        self._x = x
-        self._y = y
 
-        self._m = len(self._x) # m 表示样本的数量
-        self._n = len(self._x[0]) # n 表示特征的数量
-        self._alpha = [0] * self._m
+def get_max_class(labels):
+    classes = set(labels)
+    class_group = []
+    for c in classes:
+        class_group.append(list(labels).count(c))
+    max_feature_index = np.argsort(class_group)[-1]
+    return list(classes)[max_feature_index]
 
-    def _clip_alpha2_new(self, alpha2_new, low, high):
-        if high < low:
-            raise 'high不能小于low，程序有错，high：%.2f，low：%.2f' % (high, low)
-        # 【p127，公式7.108】
-        if alpha2_new > high:
-            return high
-        elif alpha2_new >= low and alpha2_new <= high:
-            return alpha2_new
+test_labels = [2, 3, 3, 2, 1, 1, 1, 1]
+class_name = get_max_class(test_labels)
+assert 1 == class_name
+
+def c45(features, labels, epsilon=0.1):
+    assert len(features) > 0
+    assert len(features) == len(labels)
+    length = len(labels)
+    classes = set(labels)
+    # 【p65，算法5.3第（1）步】
+    if len(classes) == 1:
+        return Node(features, labels, None, classes.pop())
+    # 【p65，算法5.3第（2）步】获取实例数最大的类
+    if len(features[0]) == 0:
+        max_class = get_max_class(labels)
+        return Node(features, labels, None, max_class)
+    # 【p65，算法5.3第（3）步】选择信息增益比最大的特征
+    max_feature_index, max_feature_gain = get_max_information_gain_ratio(features, labels)
+    # 【p65，算法5.3第（4）步】信息增益小于阈值
+    if max_feature_gain < epsilon:
+        max_class = get_max_class(labels)
+        return Node(features, labels, None, max_class)
+    # 【p65，算法5.3第（5）（6）步】构建多叉树的节点
+    feature_indexes = list(range(features.shape[1]))
+    feature_indexes.remove(max_feature_index) # 第（6）步，从数据集中剔除当前特征
+    minus_features = features[:,feature_indexes]
+    max_feature = features[:, max_feature_index] # 按增益最大的特征分割数据集
+    max_classes = set(max_feature)
+    # 按类型对数据集进行分割
+    node = Node(features, labels, max_feature_index)
+    for c in max_classes:
+        indexes = [i for i in range(len(max_feature)) if max_feature[i] == c]
+        print('feature:%s' % c)
+        print(minus_features[indexes])
+        print(labels[indexes])
+        child_node = c45(minus_features[indexes], labels[indexes])
+        # pdb.set_trace()
+        node.children[c] = child_node
+    return node
+
+
+# 测试函数c45，第1个if分支
+test_features = np.array([[], [], []])
+test_labels = np.array([0, 0, 0])
+test_tree = c45(test_features, test_labels)
+assert isinstance(test_tree, Node)
+assert test_tree.is_leaf()
+
+# 测试函数c45，第2个if分支
+test_features = np.array([[], [], []])
+test_labels = np.array([0, 1, 1])
+test_tree = c45(test_features, test_labels)
+assert isinstance(test_tree, Node)
+assert test_tree.is_leaf()
+assert 101, test_tree.pre_label
+
+# 测试函数c45，第3个if分支，使用前面整理好的数据：x_data和y_data
+test_tree = c45(x_data, y_data, 0.45) # epsilon取0.45，让特征A(3)的增益0.42小于这个阈值
+assert isinstance(test_tree, Node)
+assert test_tree.is_leaf()
+assert 101 == test_tree.pre_label
+
+# 测试函数c45，剩余分支，使用前面整理好的数据：x_data和y_data
+test_tree = c45(x_data, y_data, epsilon=0.1) # epsilon取默认值0.1
+assert isinstance(test_tree, Node)
+assert False == test_tree.is_leaf()
+
+print('*'*8)
+print(test_tree.sub_space)
+print(test_tree.labels)
+
+
+
+
+import queue
+def get_leaves(node):
+    # 层遍历树（用队列实现层遍历），获取所有的叶子节点
+    que = queue.Queue()
+    leaves = []
+    que.put(node)
+    while not que.empty():
+        node = que.get()
+        if node.is_leaf():
+            leaves.append(node)
         else:
-            return low
+            for key in node.children.keys():
+                que.put(node.children[key])
+    return leaves
 
-    def _gx(self, index):
-        # 【p127，公式7.104】gx
-        gxi = self._b
-        for j in range(self._m): # 为了与书中对应，使用j作为迭代变量
-            gxi += self._alpha[j] * self._y[j] * self._k(self._x[j], self._x[index])
-        return gxi
+def set_leaves_all_soft(leaves):
+    # 设置所有叶子节点的状态为待处理：0
+    # 不可修剪状态为：1
+    new_leaves = []
+    for node in enumerate(leaves):
+        new_leaves.append((node, 0))
+    return new_leaves
 
-    def _k(self, xi, xj):
-        """线性核
-        """
-        return float(np.dot(xi, xj))
-
-    def _error(self, index):
-        # 【p127，公式7.105】单个样本的误差
-        return self._gx(index) - self._y[index]
-
-    def _is_satisfy_kkt(self, index):
-        """
-        检查是否满足KKT条件。按照论文第8页最后一段的说法，epsilon一般取0.001，会加速收敛的过程。
-        """
-        ygx = self._y[index] * self._gx(index)
-        # 【p128，公式7.111】-epsilon <= alpha <= epsilon
-        if self._alpha[index] >= -self._epsilon and self._alpha[index] <= self._epsilon:
-            return ygx >= 1
-        # 【p129，公式7.112】epsilon < alpha < C-epsilon
-        elif self._alpha[index] > self._epsilon and self._alpha[index] < self._c - self._epsilon: 
-            return ygx == 1
-        # 【p129，公式7.113】C-epsilon <= alpha <= C+epsilon
-        elif self._alpha[index] >= self._c - self._epsilon and self._alpha[index] <= self._c + self._epsilon:
-            return ygx <= 1
-        else:
-            raise 'alpha取到了异常值:%.3f，alpha的取值范围应该是：%.3f<=alpha<=%.3f。' \
-                % (self._alpha[index], -self._epsilon, self._c + self._epsilon)
-
-    def _is_stop(self):
-        """
-        检查是否所有的样本都满足KKT条件。如果，都满足，则返回True，即停机并返回最终的结果。否则，返回False。
-        【p130，算法7.5，第（3）步】停机条件
-        """
-        for i in range(self._m):
-            if not self._is_satisfy_kkt(i):
-                return False
+def is_stop(leaves):
+    # 如果全部得叶子节点全都不能再修剪，则停止修剪算法
+    for node in enumerate(leaves):
+        # 如果有一个节点可以修剪，则直接返回False
+        if node[0] == 0:
+            return False
+    else:
         return True
 
-    def _choose_second_parameter(self, first_parameter_index):
-        """
-        选择第2个变量。【p129，2. 第2个变量的选择】
-        """
-        # # 先计算全部样本的error
-        # errors = [self._error(i) for i in range(self._m)]
-        # # 再计算E1与每个样本的E
-        # error_diffs = [abs(errors[first_parameter_index] - errors[i]) for i in range(self._m)]
-        # # 排序，并选择最大的error_diff
-        # sorted_error_diff_indexes = np.argsort(error_diffs)
-        # # 如果error_diffs最大的那个变量是第1个变量本身，则选择第2大的那个变量
-        # if sorted_error_diff_indexes[-1] == first_parameter_index:
-        #     return int(sorted_error_diff_indexes[-2])
-        # else:
-        #     return int(sorted_error_diff_indexes[-1])
-        entire = list(range(self._m))
-        entire.remove(first_parameter_index)
-        i = int(random.random() * len(entire))
-        return entire[i]
-        # return min(first_parameter_index + 1, self._m)
+leaves = get_leaves(test_tree)
+assert len(leaves) == 3
 
-    def _choose_parameters(self):
-        indexes = list(range(self._m))
-        # 先从0<alpha<C对应的样本中选择第1个不符合KKT条件的样本，并作为第1个变量。
-        # 1. 【p128，最后一段说，“选取违反KKT条件最严重的样本点”，因为没有看懂如何判断最严重，
-        # 所以，本程序中，直接选择第1个违反KKT条件的样本。】
-        # 2. 【SMO论文p10、p11中，说随机选择一个违反KKT条件样本，本程序并没有按这种方式实现。】
-        condidate_indexes = list(filter(lambda i: self._alpha[i] > 0 and self._alpha[i] < self._c, indexes))
-        for i in condidate_indexes:
-            if self._is_satisfy_kkt(i):
-                continue
-            p2 = self._choose_second_parameter(i)
-            return i, p2
 
-        # 如果搜索的0<alpha<C对应的样本都满足KKT条件，那么从剩余的样本中选择第1个不符合KKT条件的样本，
-        # 并作为第1个变量。
-        remaining_indexes = list(set(indexes) - set(condidate_indexes))
-        for i in remaining_indexes:
-            if self._is_satisfy_kkt(i):
-                continue
-            i2 = self._choose_second_parameter(i)
-            return i, i2
+def tree_loss(root_node, alpha):
+    pass
 
-x_data = [
-    [14, 13],
-    [4, 3],
-    [3, 3],
-    [2.5, 2.5],
-    [2, 2],
-    [1.5, 1.5],
-    [1, 1]
-]
-y_data = [
-    1,
-    1,
-    1,
-    -1,
-    -1, 
-    -1,
-    -1]
-x_val = [
-    [0, 0],
-    [0, 2],
-    [2, 0],
+def pruning_node():
+    pass
 
-    [0, 4],
-    [4, 0],
+def pruning(node):
+    max_interaction = 100
+    for i in range(max_interaction):
+        leaves = get_leaves(node)
+        leaves = set_leaves_all_soft(leaves)
+        if is_stop(leaves):
+            return node
+        for leaf in leaves:
+            pass
+            # 当前树的损失函数
+            # leaf这个节点的父节点剪枝之后的损失函数
+            # 比较损失函数：
+            # 之前大于之后：剪枝
+            # 否则，设置这个父节点对应的这几个叶子节点的状态都是1
 
-    [0, 6],
-    [6, 0]
-]
-y_val = [-1, -1, -1, 1, 1, 1, 1]
-
-smo = Smo()
-try:
-    smo.train(x_data, y_data)
-    y_pre = smo.predict(x_val)
-except Exception as ex:
-    print(ex)
-print(y_pre)
-print(y_val)
+pruning(test_tree)
