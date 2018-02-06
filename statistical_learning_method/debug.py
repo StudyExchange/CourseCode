a6e939e339a01dcb10fcfacd0b6c9d38eeb77c8f
se <StudyExchange@163.com>
2018-02-07 00:20:34 +0800

    [se] statistical_learning_method, algorithm 10.2(forward stagewise algorithm of observation sequence)

--- statistical_learning_method/debug.py
+++ statistical_learning_method/debug.py
@@ -1,45 +1,32 @@
 import math
 
-class MarkovDirectly(object):
+class MarkovForward(object):
     def probability(self, a, b, pi, output):
-        an = len(a[0]) # a的状态的数量
+        an = len(a[0]) # a的状态的数量 = len(pi)
         bn = len(b[0]) # b的状态的数量
         m = len(output) # 观测序列的长度
-        inputs = []
-        total_count = int(math.pow(an, m))
-        result = 0
-        for i in range(total_count): # i表示总的循环数
-            # for j in range(an): # j表示pi的index
-                # index_li = [j]
-            index_li = MarkovDirectly.get_sequence_index(i, an, m)
-            print('q_t的集合：%s' % index_li, end='。') # 即本次循环的状态集合，即本次循环的选中的盒子的集合
-            prob = pi[index_li[0]] # pi(i1)
-            for k in range(m - 1):
-                prob *= b[index_li[k]][output[k]] # b_i1(o1), b_i2(o2)
-                print('i%s->i%s' % (k, k+1), end='  ')
-                prob *= a[index_li[k]][index_li[k+1]] # a_i1_i2, a_i2_i3
-            prob *= b[index_li[m - 1]][output[m - 1]] # b_i3(o3)
-            print('  %.5f' % prob)
-            result += prob
+        
+        alphas = []
+        alpha1 = [0] * an
+        # alpha1
+        for i in range(an):
+            alpha1[i] = pi[i] * b[i][output[0]]
+        print(alpha1)
+        alphas.append(alpha1)
+        # alpha2到alpha_T
+        for i in range(1, m):
+            alpha = [0] * an
+            for j in range(an):
+                alpha[j] = 0
+                for k in range(an):
+                    alpha[j] += alphas[-1][k] * a[k][j]
+                print('%.3f x %.3f' % (alpha[j], b[j][output[i]]), end=' = ')
+                alpha[j] *= b[j][output[i]]
+                print('%.4f' % alpha[j])
+            alphas.append(alpha)
+        
+        result = sum(alphas[-1])
         return result
-
-
-    @staticmethod
-    def get_sequence_index(num, an, m):
-        if num:
-            index_li = []
-            while num:
-                quotient, remainder = divmod(num, an)
-                num = quotient
-                index_li.append(remainder)
-            if len(index_li) < m:
-                index_li.extend([0]*(m - len(index_li)))
-            return index_li
-        else:
-            return [0]*m
-
-    def is_skip(self, indexes):
-        pass
 
 
 # 测试数据1：测试数据：p177，例10.2（主要的测试数据）
@@ -56,7 +43,7 @@ b = [
 pi = [0.2, 0.4, 0.4]
 output = [0, 1, 0]
 
-mf = MarkovDirectly()
+mf = MarkovForward()
 print(mf.probability(a, b, pi, output))
 
 
@@ -76,6 +63,6 @@ b = [
 pi = (0.25, 0.25, 0.25, 0.25)
 output = [0, 0, 1, 1, 0]
 
-mf = MarkovDirectly()
+mf = MarkovForward()
 print(mf.probability(a, b, pi, output))
 
